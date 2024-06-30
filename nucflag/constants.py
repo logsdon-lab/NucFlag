04e5bee375f02bc661ac9ce2906c2aa90fce27a0
koisland <koshima789@gmail.com>
2024-06-30 19:45:44 -0400

    Add misjoin threshold param. Fix clamping valley peak finding height threshold if negative. Replace valley min threshold with multiple points. Update default configuration. Remove config replacement.

--- nucflag/constants.py
+++ nucflag/constants.py
@@ -6,6 +6,4 @@ PLOT_HEIGHT = 6
 PLOT_WIDTH = 16
 PLOT_DPI = 600
 PLOT_YLIM = 100
-# TODO: Dunno if this should be configurable?
-THR_MISJOIN_VALLEY_HEIGHT_PERC_BELOW = 0.000001
 RGX_REGION = re.compile(r"(.+):(\d+)-(\d+)")
